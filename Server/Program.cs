e026deb688616e34c4db89930432da61db169cba
Hugo <hugo.frost@live.se>
2019-05-25 16:37:52 +0200

    Initial commit

---
+++ Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsynchronousSocketListener server = new AsynchronousSocketListener();
+            Console.WriteLine("Hello World!");
+        }
+    }
+
